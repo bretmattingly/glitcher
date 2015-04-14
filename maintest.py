624307868b373f3bba78a1fcf5ea32b7e99ff720
bomattin <bomattin@butler.edu>
2015-04-14 16:53:30 -0400

    Fully implemented Horizontal sort

--- maintest.py
+++ maintest.py
@@ -5,3 +5,52 @@ __version__ = 'v0.1'
 A simple experiment in databending some images.
 '''
 
+from PIL import Image
+from random import shuffle
+
+def horizontal_shuffle(image):
+    width, height = image.size
+    evendivisors = []
+    for i in range(5, height):
+        if height % i == 0:
+            evendivisors.append(i)
+    pass
+
+    print("Even divisors are: ")
+    print(evendivisors)
+    chosendivisor = int(input("Choose your divisor: "))
+    print(chosendivisor)
+    numbars = int(height/chosendivisor)
+    barheight = chosendivisor
+    bars = []
+    wet = Image.new(image.mode, image.size)
+
+    for j in range(0, numbars):
+        barleft = 0
+        barupper = (barheight * j)
+        barright = int(width)
+        barlower = barupper + barheight - 1
+        print((barleft, barupper, barright, barlower))
+        box = (barleft, barupper, barright, barlower)
+        bar = image.crop(box)
+        bar.load()
+        bars.append(bar)
+        # bar.save("wet/bar" + str(j) + ".jpg") # debugging only
+        # Make a bar, put it an array, shuffle the array, reprint.
+
+    shuffle(bars)
+    for k, bar in enumerate(bars):
+        corner = (0, k*barheight)
+        wet.paste(bar, corner)
+
+    wet.save("wet/final.jpg")
+try:
+    im = Image.open("dry/aztec_eagle.jpg")
+except IOError:
+    print("File not found, you done messed up")
+    exit()
+
+print(im.format, im.size, im.mode)
+# im.show()
+horizontal_shuffle(im)
+
